c898f7e06cfb6a4524b9af42a292f076b0dde607
Caden-S <31747023+Caden-S@users.noreply.github.com>
2023-02-22 12:35:15 -0500

    Remove local file locations

--- Pictures.py
+++ Pictures.py
@@ -20,9 +20,9 @@ f.write("\n")
 
 for letter in alphabet:
   IMAGE_FILES = []
-  for filename in glob.iglob("C:\\Users\\Caden\\Desktop\\Hands\\asl_train\\{}\\".format(letter) + '*.png', recursive=True):
+  for filename in glob.iglob("asl_train\\{}\\".format(letter) + '*.png', recursive=True):
     IMAGE_FILES.append(filename)
-  for filename in glob.iglob("C:\\Users\\Caden\\Desktop\\Hands\\asl_train\\{}\\".format(letter) + '*.jpg', recursive=True):
+  for filename in glob.iglob("asl_train\\{}\\".format(letter) + '*.jpg', recursive=True):
     IMAGE_FILES.append(filename)
   with mp_hands.Hands(
       static_image_mode=True,
